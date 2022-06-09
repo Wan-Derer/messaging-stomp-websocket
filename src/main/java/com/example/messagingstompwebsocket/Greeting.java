956512538f650974f71f989915eccaa89ccbcde0
Wan-Derer <frd-ru@yandex.ru>
2022-06-09 18:43:24 +0300

    Initial commit

---
+++ src/main/java/com/example/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,15 @@
+package com.example.messagingstompwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Greeting {
+  private String content;
+
+
+}
---
+++ src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -0,0 +1,18 @@
+/*
+https://spring.io/guides/gs/messaging-stomp-websocket/
+https://habr.com/ru/post/187822/
+ */
+
+package com.example.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingStompWebsocketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+  }
+
+}
